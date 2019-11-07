2b8f0d6f2d5db926bc46ff5d8273f78545f1498d
alinutzaB <alinutza.ms@gmail.com>
2019-11-07 23:04:25 +0200

    Updates

---
+++ precache-manifest.7a6d89b03ee3ca6af1bb143ea7c6f157.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b62bd89c82620818ab8f56072016144",
+    "url": "/productTableReact/index.html"
+  },
+  {
+    "revision": "4b63fb3d22becf8d9c47",
+    "url": "/productTableReact/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "080ac8a483f8bd86e3bc",
+    "url": "/productTableReact/static/js/2.ef5c08fd.chunk.js"
+  },
+  {
+    "revision": "4b63fb3d22becf8d9c47",
+    "url": "/productTableReact/static/js/main.b732ce92.chunk.js"
+  },
+  {
+    "revision": "279c8bca6c30ff9dbb76",
+    "url": "/productTableReact/static/js/runtime-main.c5c59290.js"
+  }
+]);
